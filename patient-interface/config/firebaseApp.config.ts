0bec67d25048c613a733b0a4d045a8de9e30ccb4
Andrii Demchenko <demch.andrew@gmail.com>
2023-11-11 17:27:49 +0100

    feat. [front][auth] Created a basic UI for the login page and functions for firebase auth operations #2

---
+++ patient-interface/config/firebaseApp.config.ts
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCggGym2fh08LH-QWR9xHdJuTsIR7ylWws",
+    authDomain: "toothfix-5a4df.firebaseapp.com",
+    projectId: "toothfix-5a4df",
+    storageBucket: "toothfix-5a4df.appspot.com",
+    messagingSenderId: "126902374578",
+    appId: "1:126902374578:web:fbfaab1460d193fdb6b9ab",
+    measurementId: "G-WG89QGY1XG"
+};
