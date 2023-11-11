d645db98073a80afe14492eb907a3effe6f3621a
gusmojsma <gusmojsma@student.gu.se>
2023-11-11 21:43:04 +0100

    #1 Changed bg & styled laanding blurb

--- patient-interface/src/app/home/page.tsx
+++ patient-interface/src/app/home/page.tsx
@@ -5,15 +5,22 @@ const home = () => {
   return (
     <main className="flex flex-col min-h-screen items-center">
       <div className="h-16 bg-white min-w-full"></div>
-      <div className="flex flex-col z-10 min-h-screen w-full items-center p-20">
-        <div className="flex flex-row w-full items-center">
+      <div className="flex flex-col z-10 min-h-screen w-full items-center p-32">
+        <div className="flex flex-row w-full items-center justify-between">
           <div className="flex flex-col items-center">
-            <div className="flex flex-col items-start w-[515px]">
-              <p className="font-sans font-bold text-5xl">Schedule with ease,</p>
-              <p className="font-sans font-bold text-5xl">smile with confidence</p>
+            <div className="flex flex-col items-start w-[40rem]">
+              <p className="font-sans font-bold text-6xl">Schedule with ease,</p>
+              <p className="font-sans font-bold text-6xl mt-1">smile with confidence</p>
+              <p className="font-sans font-bold text-4xl mt-5">book {' '} 
+              <a className="group text-[#61B7DB] transition-all duration-300 ease-in-out" href="#">
+                <span className="bg-left-bottom bg-gradient-to-r from-[#004D70] to-[#004D70] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                  now
+                </span>
+              </a>
+              </p>
             </div>
           </div>
-          <div className="ml-4">
+          <div>
             <Image
               src="/images/landing-svg.svg"
               alt="Dentist Tools"
