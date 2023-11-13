b3488f1024f58c0411ad85a10d482407b406b774
Andrii Demchenko <demch.andrew@gmail.com>
2023-11-13 14:09:59 +0100

    feat. [front][auth] Added login and registration with email and password #4

--- patient-interface/src/app/register/page.tsx
+++ patient-interface/src/app/register/page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 import {
   signInWithGoogle,
   auth,
-  logInWithEmailAndPassword
+  registerWithEmailAndPassword
 } from '../firebase';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -13,15 +13,28 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, loading, error] = useAuthState(auth);
-
-  return (
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+  
+    return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="displayName">
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="displayName"
+              type="displayName"
+              onChange={(e) => setDisplayName(e.target.value)}
+              placeholder="John Doe"
+            />
+          </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
               Email
@@ -32,7 +45,6 @@ export default function Login() {
               type="email"
               onChange={(e) => setEmail(e.target.value)}
               placeholder="youremail@email.com"
-              required
             />
           </div>
           <div className="mb-6">
@@ -45,14 +57,13 @@ export default function Login() {
               type="password"
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
-              required
             />
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
-              onClick={() => loginButton(email, password)}
+              onClick={() => registerButton(displayName, email, password)}
             >
               Sign In
             </button>
@@ -60,34 +71,30 @@ export default function Login() {
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button" onClick={googleButton}
             >
-              Sign In with Google
+              Register with Google
             </button>
           </div>
-          {/* regirster button */}
+          {/* login button */}
           <div className="flex items-center justify-between">
-            <p className='text-center text-black'>Don&apos;t have an account? <Link className='underline' href="/register">Register</Link> now.</p>
+            <p className='text-center text-black'>Already have an account? <Link className='underline' href="/login">Log In</Link> now.</p>
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
 function googleButton() {
-  signInWithGoogle()
+    signInWithGoogle()
 }
 
-function loginButton(email: string, password: string) {
-  // check the email format
-  // TODO: add some more checks
-  if (!email.includes('@')) {
-    alert("Invalid email format")
-    return
-  }
+function registerButton(name: string, email: string, password: string) {
+    // check the email format
+    // TODO: add some more checks
+    if (!email.includes('@')) {
+        alert("Invalid email format")
+        return
+    }
 
-  logInWithEmailAndPassword(email, password)
-}
-
-function registerButton() {
-  console.log("register button clicked")
+    registerWithEmailAndPassword(name, email, password)
 }
