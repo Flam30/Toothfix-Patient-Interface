848244ab73d979fcde14085423c53231dc1e5cf0
gusmojsma <gusmojsma@student.gu.se>
2023-11-13 12:22:57 +0100

    #1 Updated font code (qualiy of life improvement)

--- patient-interface/src/app/home/page.tsx
+++ patient-interface/src/app/home/page.tsx
@@ -7,11 +7,11 @@ const home = () => {
       <div className="h-16 bg-white min-w-full"></div>
       <div className="flex flex-col z-10 min-h-screen w-full items-center p-32">
         <div className="flex flex-row w-full items-center justify-between">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center font">
             <div className="flex flex-col items-start w-[40rem]">
-              <p className="font-sans font-bold text-6xl">Schedule with ease,</p>
-              <p className="font-sans font-bold text-6xl mt-1">smile with confidence</p>
-              <p className="font-sans font-bold text-4xl mt-5">book {' '} 
+              <p className="font-eina font-bold text-6xl">Schedule with ease,</p>
+              <p className="font-eina font-bold text-6xl mt-1">smile with confidence</p>
+              <p className="font-eina font-bold text-4xl mt-5">book {' '} 
               <a className="group text-[#61B7DB] transition-all duration-300 ease-in-out" href="#">
                 <span className="bg-left-bottom bg-gradient-to-r from-[#004D70] to-[#004D70] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                   now
