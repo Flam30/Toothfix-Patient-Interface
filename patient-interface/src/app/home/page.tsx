cd4ccc2bfc45236b65498794317944fef5f180db
Adrian Hassa <adrianhassa@gmail.com>
2023-11-13 14:48:07 +0100

    #6 Add location markers support

--- patient-interface/src/app/home/page.tsx
+++ patient-interface/src/app/home/page.tsx
@@ -4,20 +4,47 @@ import React, { useRef, useEffect } from 'react'
 import Image from 'next/image'
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
+// Default center of the map
+const mapCenter = { lat: 57.70921358199699, lng: 11.973907847754571 }
+
+// Array of markers that are displayed on the map
+const markersArray = [
+  {lat: 57.70981182062649, lng: 11.939436298143544},
+  {lat: 57.70061334271214, lng: 11.954265175470795}
+]
+
+export const addMarkers = ({
+  markers,
+  map,
+}: {
+  markers: ReadonlyArray<google.maps.LatLngLiteral>;
+  map: google.maps.Map | null | undefined;
+}) =>
+  markers.map(
+    position =>
+      new google.maps.Marker({
+        position,
+        map,
+      }),
+  );
+
 function MapComponent({
   center,
   zoom,
+  markers
 }: {
   center: google.maps.LatLngLiteral;
   zoom: number;
+  markers: ReadonlyArray<google.maps.LatLngLiteral>
 }) {
   const ref:any = useRef();
 
   useEffect(() => {
-    new window.google.maps.Map(ref.current, {
+    const map = new window.google.maps.Map(ref.current, {
       center,
       zoom,
     });
+    addMarkers({ markers, map });
   });
 
   return <div ref={ref} id="map" style={{ width: "800px", height: "500px" }} />;
@@ -28,8 +55,6 @@ const render = (status: Status): any => {
   if (status === Status.FAILURE) return <h3>{status}</h3>;
   return null;
 };
-
-const mapCenter = { lat: 57.70921358199699, lng: 11.973907847754571 }
 
 const home = () => {
   return (
@@ -62,7 +87,7 @@ const home = () => {
         </div>
       </div>
       <Wrapper apiKey={"AIzaSyAFkQtGnPJcOIjEQHqH52LrCPB1uSDP1uk"} render={render}>
-        <MapComponent center={ mapCenter } zoom={10}/>
+        <MapComponent center={ mapCenter } zoom={12} markers={ markersArray }/>
       </Wrapper>
     </main>
   )
