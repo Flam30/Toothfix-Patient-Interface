b98d29faefb4b962dce7fe610cd21db5d7cd3a8b
Marcelo <gussanma@student.gu.se>
2023-11-15 22:15:20 +0100

    #3 Navbar is functional and styled, links missing

--- patient-interface/src/app/components/Navbar.tsx
+++ patient-interface/src/app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
     return (
         //TODO change bg color to match theme
         <nav className="bg-white flex items-center justify-between p-4 min-w-full">  
-              <div className="flex items-center">
+              <div className="flex items-center transition ease-out duration-300 hover:w-[260px]">
                 {/* TODO: make image clickable to redirect you to the main */}
+                <Link href="/">
                     <Image
                      className="ml-2 justify-start"
                      src="/toothfix.svg" 
@@ -14,11 +16,12 @@ const Navbar: React.FC = () => {
                      width= {275}
                      height= {61}
                      />
+                </Link>
                 </div>
-                <ul className="flex items-center space-x-9 cursor-auto mr-5">
-                    {/* TODO add links */}
-                    <li><a href="https://http.dog/204.jpg">Book</a></li>
-                    <li><a href="https://http.dog/204.jpg">My pages</a></li>
+                <ul className="flex items-center space-x-9 cursor-auto mr-5 font-bebas text-[2.9rem] text-text">
+                    {/* TODO add links and hover animations maybe*/}
+                    <li className=" hover:text-primary"><Link href="https://http.dog/204.jpg">Book</Link></li>
+                    <li className=" hover:text-primary"><Link href="https://http.dog/204.jpg">My pages</Link></li>
                 </ul> 
             </nav>
     )
