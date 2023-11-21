37cf92d63a5ae9e69e4f4d191aae4292b87b0f36
Andrii Demchenko <demch.andrew@gmail.com>
2023-11-21 17:08:11 +0100

    feat. [front] Changed the text on the register button #4

--- patient-interface/src/app/register/page.tsx
+++ patient-interface/src/app/register/page.tsx
@@ -65,7 +65,7 @@ export default function Register() {
               type="button"
               onClick={() => registerButton(displayName, email, password)}
             >
-              Sign In
+              Sign Up
             </button>
             <button
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
