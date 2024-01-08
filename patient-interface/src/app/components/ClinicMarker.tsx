0975e72d3b2ad56a34ea61ff246bccf4345e4aae
Adrian Hassa <adrianhassa@gmail.com>
2024-01-08 20:07:55 +0100

    #16 Add dynamic marker loading

--- patient-interface/src/app/components/ClinicMarker.tsx
+++ patient-interface/src/app/components/ClinicMarker.tsx
@@ -1,14 +1,27 @@
-import React, {useState} from 'react';
-import { AdvancedMarker, InfoWindow, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
+import React, { useState } from "react";
+import {
+  AdvancedMarker,
+  InfoWindow,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
+import { useRouter } from "next/navigation";
 
 export interface ClinicMarkerProps {
-    position: google.maps.LatLngLiteral;
-    name: string;
-  }
+  position: google.maps.LatLngLiteral;
+  name: string;
+  address: string;
+  hours: string;
+}
 
-export const ClinicMarker = ({ position, name }: ClinicMarkerProps): JSX.Element => {
+export const ClinicMarker = ({
+  position,
+  name,
+  address,
+  hours,
+}: ClinicMarkerProps): JSX.Element => {
   const [isInfoVisible, setIsInfoVisible] = useState(false);
   const [markerRef, marker] = useAdvancedMarkerRef();
+  const router = useRouter();
 
   return (
     <>
@@ -22,9 +35,21 @@ export const ClinicMarker = ({ position, name }: ClinicMarkerProps): JSX.Element
         <InfoWindow
           anchor={marker}
           maxWidth={200}
-          onCloseClick={() => setIsInfoVisible(false)}>
-          <div className='text-black my-1'>{name}</div>
-          <button className='text-white bg-blue-600 p-1 rounded'>Book</button>
+          onCloseClick={() => setIsInfoVisible(false)}
+        >
+          <div className="text-black my-1">
+            <b>{name}</b>
+          </div>
+          <div className="text-black my-1">{address ? address : "Address"}</div>
+          <div className="text-black my-1">
+            {hours ? hours : "09:00 AM - 5:00 PM"}
+          </div>
+          <button
+            className="text-white bg-blue-600 p-1 rounded"
+            onClick={() => router.push("/clinicselect")}
+          >
+            Book
+          </button>
         </InfoWindow>
       )}
     </>
--- patient-interface/src/app/components/MapComponent.tsx
+++ patient-interface/src/app/components/MapComponent.tsx
@@ -1,26 +1,46 @@
-import {APIProvider, Map, Marker, AdvancedMarker, InfoWindow, useAdvancedMarkerRef} from '@vis.gl/react-google-maps';
-import React from 'react';
-import { ClinicMarker, ClinicMarkerProps } from './ClinicMarker';
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import React, { useEffect, useState } from "react";
+import { ClinicMarker, ClinicMarkerProps } from "./ClinicMarker";
+import axios from "axios";
 
 export function MapComponent() {
+  const API_URL = "http://localhost:3005";
   const mapCenter = { lat: 57.70921358199699, lng: 11.973907847754571 };
-  const markers : ReadonlyArray<ClinicMarkerProps> = [
-    {
-      position: {lat: 57.70981182062649, lng: 11.939436298143544},
-      name: "Karlatornet"
-    },
-    {
-      position: {lat: 57.70061334271214, lng: 11.954265175470795},
-      name: "Pustervik"
-    }
-  ]
+  const [markers, setMarkers] = useState<ReadonlyArray<ClinicMarkerProps>>([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/booking/clinics/`)
+      .then((res) => {
+        // for each object in res.data, create a marker
+        let newMarkers: ClinicMarkerProps[] = [];
+        res.data.forEach((clinic: any) => {
+          newMarkers.push({
+            position: { lat: clinic.lat, lng: clinic.lng },
+            name: clinic.name,
+            address: clinic.address,
+            hours: clinic.hours,
+          });
+          setMarkers(newMarkers);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="w-full h-[280px] sm:h-[450px] md:h-[550px]">
-      <APIProvider apiKey={'AIzaSyAFkQtGnPJcOIjEQHqH52LrCPB1uSDP1uk'}>
-        <Map center={mapCenter} zoom={12} mapId={'clinicMap'}>
+      <APIProvider apiKey={"AIzaSyAFkQtGnPJcOIjEQHqH52LrCPB1uSDP1uk"}>
+        <Map center={mapCenter} zoom={12} mapId={"clinicMap"}>
           {markers.map((marker: ClinicMarkerProps, i) => (
-            <ClinicMarker position={marker.position} name={marker.name} key={i} />
+            <ClinicMarker
+              position={marker.position}
+              name={marker.name}
+              address={marker.address}
+              hours={marker.hours}
+              key={i}
+            />
           ))}
         </Map>
       </APIProvider>
